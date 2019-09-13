219837d12f9ec54c8aa7ed0f6520bc36ab9e2e5c
jovikpw <jovikpw@gmail.com>
2019-09-13 18:02:32 +0500

    React Structure + Ongoing Router #3

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 export default class Header extends Component {
     render() {
         return (
+            <Router>
             <div>
-                This is a header!
+                <Link to="/">Home</Link>
+                <Link to="/">About Us</Link>
+
+                <Route path="/" component={Home} />
+                <Route path="/about" component={About} />
             </div>
+            </Router>
         );
     }
 }
